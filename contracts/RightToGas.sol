00d717f0de73c3df145a261c387d1955775487aa
Theo Turner <theo_turner@outlook.com>
2019-11-29 15:00:48 +0000

    Safety on msg sender over tx origin

--- contracts/RightToGas.sol
+++ contracts/RightToGas.sol
@@ -2,11 +2,7 @@ pragma solidity >=0.4.22 <0.6.0;
 
 import "./ConvertLib.sol";
 
-// This is just a simple example of a coin-like contract.
-// It is not standards compatible and cannot be expected to talk to other
-// coin/token contracts. If you want to create a standards-compliant
-// token, see: https://github.com/ConsenSys/Tokens. Cheers!
-
+// Implement standard compliance of tokens: https://github.com/ConsenSys/Tokens
 
 contract RightToGas {
 	mapping (address => uint) balances;
@@ -14,7 +10,7 @@ contract RightToGas {
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
 	constructor() public {
-		balances[tx.origin] = 10000;
+		balances[msg.sender] = 10000;
 	}
 
 	function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
