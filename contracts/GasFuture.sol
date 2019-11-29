c65caf57667c834e99488d8f38326b25e8e271b3
Theo Turner <theo_turner@outlook.com>
2019-11-29 12:08:32 +0000

    Construct buyer/seller

--- contracts/GasFuture.sol
+++ contracts/GasFuture.sol
@@ -2,11 +2,16 @@ pragma solidity >=0.4.22 <0.6.0;
 
 contract GasFuture {
 
+    address public buyer;
+    address public seller;
+
     mapping (address => uint) balances;
 
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-	constructor() public {
+	constructor(address buyer_address, address seller_address) public {
+        buyer = buyer_address;
+        seller = seller_address;
 	}
 
 }
