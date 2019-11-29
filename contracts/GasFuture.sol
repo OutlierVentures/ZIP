352414e20f8babb6d62e23cec287b84ebf922cf8
Theo Turner <theo_turner@outlook.com>
2019-11-29 11:54:28 +0000

    Balance mapping and trandfer event

--- contracts/GasFuture.sol
+++ contracts/GasFuture.sol
@@ -2,6 +2,10 @@ pragma solidity >=0.4.22 <0.6.0;
 
 contract GasFuture {
 
+    mapping (address => uint) balances;
+
+	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
 	constructor() public {
 	}
 
