6d640654d60a5ee05497581bab6de175cdd57209
Theo Turner <theo_turner@outlook.com>
2019-11-29 12:50:35 +0000

    Buyer and seller are nopw payables

--- contracts/GasFuture.sol
+++ contracts/GasFuture.sol
@@ -2,8 +2,8 @@ pragma solidity >=0.4.22 <0.6.0;
 
 contract GasFuture {
 
-    address public buyer;
-    address public seller;
+    address payable public buyer;
+    address payable public seller;
     uint private price;
     uint private start;
     uint private length_days;
@@ -15,7 +15,7 @@ contract GasFuture {
 
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-	constructor(address buyer_address, address seller_address, uint agreed_price, uint contract_length_days) public {
+	constructor(address payable buyer_address, address payable seller_address, uint agreed_price, uint contract_length_days) public {
         start = block.timestamp;
         buyer = buyer_address;
         seller = seller_address;
@@ -23,7 +23,7 @@ contract GasFuture {
         length_days = contract_length_days;
 	}
 
-    // The buyer and seller accept the terms by calling this function
+    // The buyer and seller accept the terms by calling this function.
     function accept() public {
         if (msg.sender == buyer){
             buyerAccepts = true;
