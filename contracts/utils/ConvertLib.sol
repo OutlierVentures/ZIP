b20f74b540c8d938ada405fa202e5c95979774cd
Theo Turner <theo_turner@outlook.com>
2020-01-13 13:46:04 +0000

    Convertlib according to rate of contracts

--- contracts/utils/ConvertLib.sol
+++ contracts/utils/ConvertLib.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.5.0;
 
 library ConvertLib{
-	function convert(uint amount,uint conversionRate) public pure returns (uint convertedAmount)
+	function convert(uint256 amount, address inputContract, address outputContract) public pure returns (uint256 convertedAmount)
 	{
-		return amount * conversionRate;
+		// TODO Oracle data feeds for price - MULTIPLY BY 100 FOR INT, HANDLE address(0) MEANS ETH
+		uint256 priceInputETH = 10;
+		uint256 priceOutputETH = 5;
+		return (priceInputETH / priceOutputETH) * amount;
 	}
 }
