0186f08ac370a3b3e80369b992fe540a837e6709
Theo Turner <theo_turner@outlook.com>
2020-01-14 11:59:10 +0000

    Core Chainlinked contract

--- contracts/utils/ConvertLib.sol
+++ contracts/utils/ConvertLib.sol
@@ -7,11 +7,11 @@ contract ConvertLib is ChainlinkClient {
 	constructor(address _link) public {
 		// Set the address for the LINK token for the network.
 		if(_link == address(0)) {
-		// Useful for deploying to public networks.
-		setPublicChainlinkToken();
+			// Useful for deploying to public networks.
+			setPublicChainlinkToken();
 		} else {
-		// Useful if you're deploying to a local network.
-		setChainlinkToken(_link);
+			// Useful if you're deploying to a local network.
+			setChainlinkToken(_link);
 		}
 	}
 
@@ -21,5 +21,35 @@ contract ConvertLib is ChainlinkClient {
 		uint256 priceInputETH = 10;
 		uint256 priceOutputETH = 5;
 		return (priceInputETH / priceOutputETH) * amount;
+	}
+
+	// Creates a Chainlink request with the uint256 multiplier job
+	function requestEthereumPrice(address _oracle, bytes32 _jobId, uint256 _payment) public onlyOwner {
+		// newRequest takes a JobID, a callback address, and callback function as input
+		Chainlink.Request memory req = buildChainlinkRequest(_jobId, this, this.fulfill.selector);
+		// Adds a URL with the key "get" to the request parameters
+		req.add("get", "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD");
+		// Uses input param (dot-delimited string) as the "path" in the request parameters
+		req.add("path", "USD");
+		// Adds an integer with the key "times" to the request parameters
+		req.addInt("times", 100);
+		// Sends the request with the amount of payment specified to the oracle
+		sendChainlinkRequestTo(_oracle, req, _payment);
+	}
+
+	// Use recordChainlinkFulfillment to ensure only the requesting oracle can fulfill. Outputs uint256.
+	function fulfill(bytes32 _requestId, uint256 _price) public recordChainlinkFulfillment(_requestId) {
+		currentPrice = _price;
+	}
+
+	// withdrawLink allows the owner to withdraw any extra LINK on the contract
+	function withdrawLink() public onlyOwner {
+		LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+		require(link.transfer(msg.sender, link.balanceOf(address(this))), "Unable to transfer");
+	}
+
+	modifier onlyOwner() {
+		require(msg.sender == owner, "Message sender must be contract owner");
+		_;
 	}
 }
