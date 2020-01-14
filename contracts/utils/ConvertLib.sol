eee4acdb9a20b84e465427c4aab21cc519e15b67
Theo Turner <theo_turner@outlook.com>
2020-01-14 11:50:42 +0000

    ConvertLib is now Chainlinked contract

--- contracts/utils/ConvertLib.sol
+++ contracts/utils/ConvertLib.sol
@@ -2,7 +2,19 @@ pragma solidity ^0.5.0;
 
 import "../../node_modules/chainlink/contracts/ChainlinkClient.sol";
 
-library ConvertLib{
+contract ConvertLib is ChainlinkClient {
+
+	constructor(address _link) public {
+		// Set the address for the LINK token for the network.
+		if(_link == address(0)) {
+		// Useful for deploying to public networks.
+		setPublicChainlinkToken();
+		} else {
+		// Useful if you're deploying to a local network.
+		setChainlinkToken(_link);
+		}
+	}
+
 	function convert(uint256 amount, address inputContract, address outputContract) public pure returns (uint256 convertedAmount)
 	{
 		// TODO Oracle data feeds for price - MULTIPLY BY 100 FOR INT, HANDLE address(0) MEANS ETH
