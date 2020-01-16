e8f961942635b806767a371825c9afb92181bf17
Theo Turner <theo_turner@outlook.com>
2020-01-16 13:18:14 +0000

    DecreaseExternalAllowance

--- contracts/token/SpendExternal.sol
+++ contracts/token/SpendExternal.sol
@@ -7,9 +7,15 @@ import "../utils/SafeMath.sol";
 contract SpendExternal is Context {
     using SafeMath for uint256;
 
-    function increaseExternalAllowance(address contractAddress, address spender, uint256 addedValue) internal returns (bool) {
+    function increaseExternalAllowance(address contractAddress, address spender, uint256 addedValue) internal {
         Interface externalToken = Interface(contractAddress);
         uint256 spenderCurrentAllowance = externalToken.allowance(_msgSender(), spender);
         externalToken.approve(spender, spenderCurrentAllowance.add(addedValue));
+    }
+
+    function decreaseExternalAllowance(address contractAddress, address spender, uint256 subtractedValue) internal {
+        Interface externalToken = Interface(contractAddress);
+        uint256 spenderCurrentAllowance = externalToken.allowance(_msgSender(), spender);
+        externalToken.approve(spender, spenderCurrentAllowance.sub(subtractedValue, "ERC20: decreased allowance below zero"));
     }
 }
