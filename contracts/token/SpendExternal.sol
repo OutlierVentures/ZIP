a08067157292feeedfec150cdb2190ea49cecdb3
Theo Turner <theo_turner@outlook.com>
2020-01-16 12:56:44 +0000

    IncreaseExternalAllowance function

--- contracts/token/SpendExternal.sol
+++ contracts/token/SpendExternal.sol
@@ -4,6 +4,12 @@ import "./Interface.sol";
 import "../utils/Context.sol";
 import "../utils/SafeMath.sol";
 
-contract SpendExternal {
+contract SpendExternal is Context {
+    using SafeMath for uint256;
 
+    function increaseExternalAllowance(address contractAddress, address spender, uint256 addedValue) internal returns (bool) {
+        Interface externalToken = Interface(contractAddress);
+        uint256 spenderCurrentAllowance = externalToken.allowance(_msgSender(), spender);
+        externalToken.approve(spender, spenderCurrentAllowance.add(addedValue));
+    }
 }
