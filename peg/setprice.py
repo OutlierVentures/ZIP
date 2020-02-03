0da13830ea63399f414b230bfd71143f87abe5e5
Theo Turner <theo_turner@outlook.com>
2020-02-03 12:37:02 +0000

    Peg: setprice pulls blockchain data

--- peg/setprice.py
+++ peg/setprice.py
@@ -1,1 +1,8 @@
 import pandas as pd
+
+class SetPrice:
+
+    def get_data(self):
+        self.gas_limit = pd.read_csv("https://etherscan.io/chart/gaslimit?output=csv")
+        self.gas_price = pd.read_csv("https://etherscan.io/chart/gasprice?output=csv")
+        self.eth_price = pd.read_csv("https://etherscan.io/chart/etherprice?output=csv")
