5735b4111b0a3a5eb42d7aac6dda8447f0ac736e
Theo Turner <theo_turner@outlook.com>
2020-02-24 12:54:16 +0000

    Update migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+const Supertoken = artifacts.require("Supertoken");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.link(ConvertLib, Supertoken);
+  deployer.deploy(Supertoken);
 };
