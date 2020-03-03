f4322726fd8756ddd9c22a849c5562a9b58e72f3
Theo Turner <theo_turner@outlook.com>
2020-03-03 12:04:45 +0000

    Mappings: updateable contract addresses

--- contracts/token/Mappings.sol
+++ contracts/token/Mappings.sol
@@ -1,9 +1,13 @@
 pragma solidity ^0.5.0;
 
 /**
- * @dev Maps token symbols to contract addresses
+ * @dev Maps token symbols to contract addresses.
  */
-contract Mappings {
+contract Mappings is OnlyOwner{
     mapping(string => address) public contractAddresses;
     mapping(string => address) public migrationAddresses;
+
+    function updateContractAddress(string symbol, address contractAddress) public {
+        contractAddresses[symbol] = contractAddress;
+    }
 }
