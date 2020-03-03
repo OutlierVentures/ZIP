1c7bc951df0723807369662b2f4aee67be0f0365
Theo Turner <theo_turner@outlook.com>
2020-03-03 14:43:14 +0000

    Mappings memory usage

--- contracts/token/Mappings.sol
+++ contracts/token/Mappings.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.5.0;
 
+import "../utils/OnlyOwner.sol"
+
 /**
  * @dev Maps token symbols to contract and migartion addreses addresses.
  */
@@ -7,21 +9,21 @@ contract Mappings is OnlyOwner {
     mapping(string => address) public contractAddresses;
     mapping(string => address) public migrationAddresses;
 
-    function getContractAddress(string symbol) public {
+    function getContractAddress(string memory symbol) public {
         require(contractAddresses[symbol].active, "Token not currently supported.");
         return contractAddresses[symbol];
     }
 
-    function setContractAddress(string symbol, address contractAddress) public {
+    function setContractAddress(string memory symbol, address contractAddress) public {
         contractAddresses[symbol] = contractAddress;
     }
 
-    function getMigrationAddress(string symbol) public {
+    function getMigrationAddress(string memory symbol) public {
         require(migrationAddresses[symbol].active, "Token not currently supported for migration.");
         return migrationAddresses[symbol];
     }
 
-    function setMigrationAddress(string symbol, address migrationAddress) public {
+    function setMigrationAddress(string memory symbol, address migrationAddress) public {
         migrationAddresses[symbol] = migrationAddress;
     }
 
