da2bcba8eeef2df8ecf07406488e88e130a05e29
Theo Turner <theo_turner@outlook.com>
2020-03-03 12:11:32 +0000

    Mappings: initial map

--- contracts/token/Mappings.sol
+++ contracts/token/Mappings.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.5.0;
 /**
  * @dev Maps token symbols to contract addresses.
  */
-contract Mappings is OnlyOwner{
+contract Mappings is OnlyOwner {
     mapping(string => address) public contractAddresses;
     mapping(string => address) public migrationAddresses;
+
+    contractAddresses["FET"] = address("0x1d287cc25dad7ccaf76a26bc660c5f7c8e2a05bd")
 
     function updateContractAddress(string symbol, address contractAddress) public {
         contractAddresses[symbol] = contractAddress;
