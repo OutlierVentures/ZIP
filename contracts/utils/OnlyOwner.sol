7d902cbdade1e7284f486f88efe86c2a9264d7b1
Theo Turner <theo_turner@outlook.com>
2020-03-03 14:37:45 +0000

    OnlyOwner has minEthBalance

--- contracts/utils/OnlyOwner.sol
+++ contracts/utils/OnlyOwner.sol
@@ -1,8 +1,13 @@
 pragma solidity ^0.5.0;
 
+/**
+* @dev Modifier for functions only the contract owner can use,
+* including core owner-only logic.
+*/
 contract OnlyOwner {
 
     address public contractOwner;
+    uint256 public _minEthBalance;
 
     /**
      * @dev Modifier for functions callable only by the contract owner.
@@ -12,5 +17,13 @@ contract OnlyOwner {
 		require(msg.sender == contractOwner, "Message sender must be contract owner");
 		_;
 	}
+
+    /**
+     * @dev Set the minimumEthBalance of the contract to cover gas.
+     * Not eligible for relayed calls.
+     */
+    function setMinEthBalance(uint256 amount) public onlyOwner {
+        _minEthBalance = amount;
+    }
 
 }
