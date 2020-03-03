c876f49c3bd10b802cfd5f252248616943ed5027
Theo Turner <theo_turner@outlook.com>
2020-03-03 11:54:51 +0000

    Define mappings

--- contracts/token/Mappings.sol
+++ contracts/token/Mappings.sol
@@ -1,2 +1,9 @@
 pragma solidity ^0.5.0;
 
+/**
+ * @dev Maps token symbols to contract addresses
+ */
+contract Mappings {
+    mapping(string => address) public contractAddresses;
+    mapping(string => address) public migrationAddresses;
+}
