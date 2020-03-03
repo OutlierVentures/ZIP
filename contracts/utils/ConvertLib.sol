310672c37126a5fadfbbd07ae4fc7e156e51415e
Theo Turner <theo_turner@outlook.com>
2020-03-03 14:37:30 +0000

    ConvertLib is standalone

--- contracts/utils/ConvertLib.sol
+++ contracts/utils/ConvertLib.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.5.0;
 
-contract ConvertLib is OnlyOwner {
+contract ConvertLib {
 
 	// Converts the amount in one token to another.
 	function convert(uint256 amount, address inputContractAddress, address outputContractAddress) public pure returns (uint256 convertedAmount) {
