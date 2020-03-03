80c95bdf44f03b525c5f83f86e496b75788c7f7b
Theo Turner <theo_turner@outlook.com>
2020-03-03 15:01:49 +0000

    Mappings: return values

--- contracts/token/Mappings.sol
+++ contracts/token/Mappings.sol
@@ -9,21 +9,21 @@ contract Mappings is OnlyOwner {
     mapping(string => address) public contractAddresses;
     mapping(string => address) public migrationAddresses;
 
-    function getContractAddress(string memory symbol) public {
+    function getContractAddress(string memory symbol) public returns (address) {
         require(contractAddresses[symbol] != address(0), "Token not currently supported.");
         return contractAddresses[symbol];
     }
 
-    function setContractAddress(string memory symbol, address contractAddress) public {
+    function setContractAddress(string memory symbol, address contractAddress) public returns (address) {
         contractAddresses[symbol] = contractAddress;
     }
 
-    function getMigrationAddress(string memory symbol) public {
+    function getMigrationAddress(string memory symbol) public returns (address) {
         require(migrationAddresses[symbol] != address(0), "Token not currently supported for migration.");
         return migrationAddresses[symbol];
     }
 
-    function setMigrationAddress(string memory symbol, address migrationAddress) public {
+    function setMigrationAddress(string memory symbol, address migrationAddress) public returns (address) {
         migrationAddresses[symbol] = migrationAddress;
     }
 
