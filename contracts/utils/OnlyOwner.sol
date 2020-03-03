f60405c7e4194dde73dd9395d2c0d7f2b29f4016
Theo Turner <theo_turner@outlook.com>
2020-03-03 12:04:28 +0000

    Add OnlyOwner contract

---
+++ contracts/utils/OnlyOwner.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.0;
+
+contract OnlyOwner {
+
+    address public contractOwner
+
+    /**
+     * @dev Modifier for functions callable only by the contract owner.
+     * Not eligible for relayed calls.
+     */
+    modifier onlyOwner() {
+		require(msg.sender == contractOwner, "Message sender must be contract owner");
+		_;
+	}
+
+}
