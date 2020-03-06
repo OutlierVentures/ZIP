25011ee4af1beaccac2bd76407d4c5899afbd804
Theo Turner <theo_turner@outlook.com>
2020-03-06 11:16:28 +0000

    DemoDapp bugfix

--- contracts/demodapp/DemoDapp.sol
+++ contracts/demodapp/DemoDapp.sol
@@ -1,25 +1,25 @@
 pragma solidity ^0.5.0;
 
 import "../token/Interface.sol";
-import "../utils/Context.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/GSN/GSNRecipient.sol";
 
-contract DemoDapp is GSNRecipient {
+// DEV NOTE: OpenZeppelin GSN Contracts already define Context, so no need to inherit the local Context contract.
+
+contract DemoDapp { //is GSNRecipient {
 
     /**
      * @dev Example dapp function: spend a base token from the basket.
      */
     function spendBaseToken() external returns (bool) {
-        private fuel = Interface("0xFUELADDRESS") // Specify FUEL address
-        fuel.transfer(address(this), 100) // Send 100 fuel to dapp
+        Inteface fuel = Interface("0xFUELADDRESS"); // Specify FUEL address
+        fuel.transfer(address(this), 100); // Send 100 fuel to dapp
         // Send 100 FUEL worth of FET to this dapp's Etch contract
-        fuel.redeem("FET", 100, "0xTHISDAPPCONTRACTFETCHADDRESS")
+        fuel.redeem("FET", 100, "0xTHISDAPPCONTRACTFETCHADDRESS");
         return true;
     }
 
-    /**
-     * @dev GSN: accept relayed call.
-     */
+    /* GSN below, see dev note above before enabling
+
     function acceptRelayedCall(
         address relay,
         address from,
@@ -34,16 +34,12 @@ contract DemoDapp is GSNRecipient {
         return _approveRelayedCall();
     }
 
-    /**
-     * @dev GSN: preprocessing for relayed calls.
-     */
     function _preRelayedCall(bytes memory context) internal returns (bytes32) {
     }
 
-    /**
-     * @dev GSN: postprocessing for relayed calls.
-     */
     function _postRelayedCall(bytes memory context, bool, uint256 actualCharge, bytes32) internal {
     }
+
+    */
 
 }
