e4dcea8a73bf75e40f411ec36aa0d0f4ec8c0750
Theo Turner <theo_turner@outlook.com>
2020-05-20 15:45:06 +0100

    Add Lock event

--- contracts/locking/Locking.sol
+++ contracts/locking/Locking.sol
@@ -2,4 +2,12 @@ pragma solidity ^0.5.0;
 
 contract Locking {
 
+    /**
+     * @dev Emitted when native token collateral is locked in this contract.
+     * Contains the `amount` plus the `totalLocked`, i.e. the amount which should exist as a wrapped token on Ethereum.
+     *
+     * Note that both `amount` and `totalLocked` may be zero.
+     */
+    event Lock(uint256 amount, uint256 totalLocked);
+    
 }
